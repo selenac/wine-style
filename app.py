93b2844c32cfbdbfef7a0c4d7c7877a2d187c517
Selena <selenalchen@gmail.com>
2017-10-27 11:49:17 -0700

    mvp of web app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
 import json
+import random
 from build_model import RecCosineSimilarity
 
 app = Flask(__name__)
@@ -13,26 +14,20 @@ with open('static/wine_df.pkl') as f:
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
-#
-# @app.route('/bybottle', methods=['GET'])
-# def bybottle():
-#     pass
-#
-# @app.route('/bydescription', methods=['GET'])
-# def bydescription():
-#     pass
-#
+    rand_idxs = random.sample(range(len(wine_df)),10)
+    wine_names = [wine_df['product'][i].decode('utf8') for i in rand_idxs]
+    return render_template('index.html', wines=wine_names)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # if inquiry == 'bybottle': #TODO add this somewhere...
-    #     wine_name = request.json['wine_name']
-    #     wine_id = wine_df.index[wine_df['product']==wine_name][0]
-    #     rec_ids = model.recommend_to_one(wine_id)
-    # else
-    data = str(request.json['user_input'])
-    rec_ids = model.recommend_user_input(data)
+    user_input = list(request.json['user_input'])
+    data = str(user_input[0])
+    rec_type = user_input[1]
+    if rec_type=='1':
+        wine_id = wine_df.index[wine_df['product']==data][0]
+        rec_ids = model.recommend_to_one(wine_id)
+    elif rec_type=='2':
+        rec_ids = model.recommend_user_input(data)
     wineList = []
     for r in rec_ids:
         wDict = {
@@ -43,20 +38,6 @@ def predict():
         wineList.append(wDict)
     recs = json.dumps(wineList, encoding='utf8')
     return jsonify({'recommendations': recs})
-
-
-    # render_template('')
-    #if recommend to one
-        #select from wine product list + submit
-        # display traits of wine selected + description
-        #return user_wine wine_id for wine submitted
-        # recs = cs.recommend_to_one(wine_id=user_wine)
-        # for idx in top_n_recs:
-        #     wine_df['product'][idx]
-        #     wine_df['province'][idx]
-        #     wine_df['country'][idx]
-        #     wine_df['price'][idx]
-        #
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
